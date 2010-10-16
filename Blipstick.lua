6d5a6ccb105660f5de14e98e9b79074e3b4adc24
tekkub <tekkub@gmail.com>
2010-10-15 23:54:03 -0600

    Fix config layout for new bigger blip textures

--- Blipstick.lua
+++ Blipstick.lua
@@ -34,8 +34,8 @@ end)
 frame.name = "Blipstick"
 frame:Hide()
 frame:SetScript("OnShow", function(frame)
-	local GAP, EDGEGAP = 8, 16
-	local ROWHEIGHT = (408-73-EDGEGAP) / #textures - GAP
+	local GAP, EDGEGAP, TEXTHEIGHT, TEXTOFFSET, HIGHLIGHTOFFSET = 8, 16, 13, 5, 8
+	local ROWHEIGHT = (408-73-EDGEGAP) / (#textures/2) - GAP
 
 	local title, subtitle = LibStub("tekKonfig-Heading").new(frame, "Blipstick", "These settings let you select a different set of minimap blips to use.")
 
@@ -45,14 +45,20 @@ frame:SetScript("OnShow", function(frame)
 		Minimap:SetBlipTexture(self.texture)
 		for _,row in pairs(rows) do row:SetChecked(row == self) end
 	end
-	for _,name in ipairs(textures) do
+	for i,name in ipairs(textures) do
+		local leftside = ((i % 2) == 1)
 		local texture = name == "Default" and DEFAULTPATH or path..name
 
 		local row = CreateFrame("CheckButton", nil, frame)
 		row:SetHeight(ROWHEIGHT)
 		row:SetPoint("TOP", anchor, "BOTTOM", 0, -GAP)
-		row:SetPoint("LEFT", frame, "LEFT", EDGEGAP, 0)
-		row:SetPoint("RIGHT", frame, "RIGHT", -EDGEGAP, 0)
+		if leftside then
+			row:SetPoint("LEFT", frame, "LEFT", EDGEGAP, 0)
+			row:SetPoint("RIGHT", frame, "CENTER")
+		else
+			row:SetPoint("LEFT", frame, "CENTER")
+			row:SetPoint("RIGHT", frame, "RIGHT", -EDGEGAP, 0)
+		end
 
 		row:SetChecked(texture == frame.db.texture)
 		row.texture = texture
@@ -61,23 +67,25 @@ frame:SetScript("OnShow", function(frame)
 		local highlight = row:CreateTexture()
 		highlight:SetTexture("Interface\\HelpFrame\\HelpFrameButton-Highlight")
 		highlight:SetTexCoord(0, 1, 0, 0.578125)
-		highlight:SetAllPoints()
 		row:SetHighlightTexture(highlight)
 		row:SetCheckedTexture(highlight)
 
 		local preview = row:CreateTexture()
-		preview:SetWidth(ROWHEIGHT*4) -- Maintain proper aspect
-		preview:SetPoint("TOPLEFT", row)
-		preview:SetPoint("BOTTOM", row)
+		preview:SetWidth((ROWHEIGHT - TEXTHEIGHT - TEXTOFFSET)*2) -- Maintain proper aspect
+		preview:SetPoint("TOP", row)
+		preview:SetPoint("BOTTOM", row, 0, TEXTHEIGHT + TEXTOFFSET)
 		preview:SetTexture(texture)
 
 		local text = row:CreateFontString(nil, nil, "GameFontHighlight")
-		text:SetPoint("LEFT", preview, "RIGHT", GAP, 0)
-		text:SetPoint("RIGHT", row)
+		text:SetPoint("TOP", preview, "BOTTOM")
 		text:SetText(name)
 
+		highlight:SetPoint("TOPLEFT", preview, -HIGHLIGHTOFFSET, HIGHLIGHTOFFSET)
+		highlight:SetPoint("RIGHT", preview, HIGHLIGHTOFFSET, 0)
+		highlight:SetPoint("BOTTOM", text, 0, -TEXTOFFSET - HIGHLIGHTOFFSET)
+
 		table.insert(rows, row)
-		anchor = row
+		anchor = not leftside and row or anchor
 	end
 
 	frame:SetScript("OnShow", nil)
